84f6db3d171121650395f9ac9c39e4e060d05bc8
Sumit Dey <dey.sumit.kr@gmail.com>
2019-06-15 22:49:46 +0200

    Project Init: 	- gitignore 	- travis-ci 	- CMakeLists 	- add library for pattern localization (dummy contents) 	- add executable (dummy tests)

---
+++ pattern_localization/PatternMap.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file PatternMap.cpp
+ * @brief 
+ * @date 2019-06-15
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
+
+#include "PatternMap.h"
+
+int getTestValue(){ return 634; }
---
+++ pattern_localization/PatternMap.h
@@ -0,0 +1,12 @@
+/**
+ * @file PatternMap.h
+ * @brief 
+ * @date 2019-06-15
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
+
+#pragma once
+
+int getTestValue();
