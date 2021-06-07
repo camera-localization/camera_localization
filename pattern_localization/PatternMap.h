4de3181c5bbe4549bf50a3cdf3413ceb8d689c4e
Anik De <anekde@gmail.com>
2021-06-07 21:43:37 +0530

    corners detected in a single image

--- pattern_localization/PatternMap.h
+++ pattern_localization/PatternMap.h
@@ -9,10 +9,14 @@
 
 #pragma once
 
+#include <iostream>
 #include <opencv2/opencv.hpp>
+#include <vector>
+#include <opencv2/core/matx.hpp>
 
 int getTestValue();
 int showImage();
 
 int showImage(std::string filePath);
+void draw_pattern(std::string path, cv::Size patternsize);
 
