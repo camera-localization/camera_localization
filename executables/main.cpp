efa1a640cda6df41486b72efa92e377112ab0f9c
Restyled.io <commits@restyled.io>
2021-06-08 15:49:53 +0000

    Restyled by astyle

--- executables/main.cpp
+++ executables/main.cpp
@@ -11,13 +11,13 @@
 #include "pattern_localization/PatternMap.h"
 
 int main() {
-	std::cout << "Hello World!" << std::endl;
-	std::cout << "Test Value: " << getTestValue() << std::endl;
-	showImage();
-	showImage("../data/camera_calibration/ps3_640x480/2019-06-23_7x9_2.8mm/2019-06-23-114903.jpg");
-	std::string image_path = "../data/camera_calibration/ps3_640x480/2019-06-23_7x9_2.8mm/2019-06-23-114903.jpg";
-	cv::Size patterndims(8,6);
-	draw_pattern(image_path, patterndims);
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "Test Value: " << getTestValue() << std::endl;
+    showImage();
+    showImage("../data/camera_calibration/ps3_640x480/2019-06-23_7x9_2.8mm/2019-06-23-114903.jpg");
+    std::string image_path = "../data/camera_calibration/ps3_640x480/2019-06-23_7x9_2.8mm/2019-06-23-114903.jpg";
+    cv::Size patterndims(8,6);
+    draw_pattern(image_path, patterndims);
 
-	return 0;
+    return 0;
 }
